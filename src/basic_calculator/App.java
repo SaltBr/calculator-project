1c8e0993698f89dccd97340e72c2cfd4232bd7b9
SaltBr <snowgriff123@gmail.com>
2025-02-26 14:55:48 +0900

    Calculator에 모든 계산 기능 이동

--- src/basic_calculator/App.java
+++ src/basic_calculator/App.java
@@ -4,12 +4,10 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) {
-        //Main
         Scanner scanner = new Scanner(System.in);
-        String [] result = {"resultNum", "errorBoolean", "errorMessage"};
+        String [] result;
 
         while(true) {
-            boolean calcError = false;
 
             //숫자 받기
             System.out.print("숫자1 입력: ");
@@ -23,15 +21,18 @@ public class App {
             System.out.print("계산 기호를 입력하세요: ");
             char calcSymbol = scanner.nextLine().charAt(0);
 
-            Calculator calculator = new Calculator(calcNum1, calcNum2, calcSymbol, result);
-            result = calculator.calculate();
+            //Calculator
+            Calculator calculator = new Calculator(calcNum1, calcNum2, calcSymbol);
+            result = calculator.getResult();
 
-            if (!calculator.result[1].equals("true")) {
-                System.out.println("\n결과: " + calculator.result[0]);
+            //결과 및 에러 출력
+            if (!result[1].equals("true")) {
+                System.out.println("\n결과: " + result[0]);
             } else {
-                System.out.println("에러! " + calculator.result[2]);
+                System.out.println("에러! " + result[2]);
             }
 
+            //종료
             System.out.println("종료하시겠습니까? (exit) ");
             String userInput = scanner.nextLine();
             if (userInput.equals("exit")) {
--- src/basic_calculator/Calculator.java
+++ src/basic_calculator/Calculator.java
@@ -4,16 +4,25 @@ public class Calculator {
     private final int calcNum1;
     private final int calcNum2;
     private final char calcSymbol;
-    String [] result;
+    private String [] result= {"0", "false", ""};
 
-    Calculator(int num1, int num2, char symbol, String[] result) {
+    Calculator(int num1, int num2, char symbol) {
         this.calcNum1 = num1;
         this.calcNum2 = num2;
         this.calcSymbol = symbol;
+    }
+
+    public String[] getResult() {
+        calculate();
+        return result;
+    }
+
+    public void setResult(String[] result) {
         this.result = result;
     }
 
-    public String [] calculate() {
+    //계산 메서드
+    public void calculate() {
         switch (calcSymbol) {
             case '+':
                 result[0] = Integer.toString(calcNum1 + calcNum2);
@@ -34,6 +43,5 @@ public class Calculator {
                     break;
                 }
         }
-        return result;
     }
 }
